1df96d6c9904971da8f66ee0f85c5c05ebe23adb
Aksel-Reistad <aksrei04@innlandetfylke.no>
2025-01-07 10:17:45 +0100

    bilde roterern funker i sin simplese form

--- bolig-invest/src/App.js
+++ bolig-invest/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import "./bilder/1.jpg";
 import "./bilder/2.jpg";
@@ -8,25 +7,9 @@ import Roterer from'./kode/BildeRoterer.js';
 function App() {
   return (
     <div className="App">
-      
-      <Roterer></Roterer>
-    
 
+      <Roterer/>
       
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- bolig-invest/src/kode/BildeRoterer.js
+++ bolig-invest/src/kode/BildeRoterer.js
@@ -1,26 +1,36 @@
-import '../App.css';
-import "../bilder/1.jpg";
-import "../bilder/2.jpg";
-import "../bilder/3.jpg";
+import React, { useEffect, useState } from 'react';
+//vis jeg fikser en dattabase husk å dra bildene derfra sammen med teksten jeg skal linke til det 
+import img1 from '../bilder/1.jpg';
+import img2 from '../bilder/2.jpg';
+import img3 from '../bilder/3.jpg';
 
+const Roterer = () => {
+  const images = [img1, img2, img3];
 
-const images = [
-    'bilder/1.jpg',
-    'bilder/2.jpg',
-    'bilder/3.jpg'
-  ];
-  
-  let currentIndex = 0;
-  
-function Roterer()
-{
-        document.body.style.backgroundImage = `url(${images[currentIndex]})`;
-        currentIndex = (currentIndex + 1) % images.length;
-}
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-setInterval(Roterer, 5000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
 
+    return () => clearInterval(interval);
+  }, [images.length]);
 
-
+  return (
+    <div
+    // fjern senere i prosjektet når jeg har ettablert sidene fordi form og størelse bør styres derfra 
+      style={{
+        height: '70vh',
+        backgroundImage: `url(${images[currentIndex]})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        transition: 'background-image 0.5s ease-in-out',
+      }}
+    >
+      {/* Ekstra innhold kan legges her */}
+    </div>
+  );
+};
 
 export default Roterer;
